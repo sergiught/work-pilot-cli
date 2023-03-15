2022006196a19d792bf84f28e8f4a4ca65ef3aa7
Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
2023-03-15 02:23:41 +0100

    Add command skeleton

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/charmbracelet/log"
+
+	"github.com/sergiught/work-pilot-cli/internal/command/base"
+)
+
 func main() {
-	
+	ctx, cancel := context.WithCancel(context.Background())
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	go func() {
+		<-ch
+		defer cancel()
+		os.Exit(0)
+	}()
+
+	log.SetLevel(log.DebugLevel)
+
+	if err := base.New().Command().ExecuteContext(ctx); err != nil {
+		log.Fatal("failed to execute command", "err", err)
+		os.Exit(1)
+	}
 }
--- internal/command/base/cmd.go
+++ internal/command/base/cmd.go
@@ -1,7 +1,9 @@
-package work
+package base
 
 import (
 	"github.com/spf13/cobra"
+
+	"github.com/sergiught/work-pilot-cli/internal/command/work"
 )
 
 type Command struct {
@@ -10,15 +12,19 @@ type Command struct {
 
 func New() *Command {
 	cmd := &cobra.Command{
-		Use:     "work",
-		Aliases: []string{"wk"},
+		Use:     "wp",
 		Short:   "",
 		Long:    "",
 		Example: "",
+		Version: "dev",
+		// SilenceErrors: true,
+		// SilenceUsage:  true,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return cmd.Help()
 		},
 	}
+
+	cmd.AddCommand(work.New().Command())
 
 	return &Command{
 		cmd: cmd,
