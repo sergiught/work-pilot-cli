771f54ba560b5520eb7a07c31177466bb3626415
Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
2023-12-28 12:24:27 +0100

    Create sqlite database in user data path

--- go.mod
+++ go.mod
@@ -29,8 +29,10 @@ require (
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mattn/go-sqlite3 v1.14.19 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/go-app-paths v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
--- internal/platform/database/database.go
+++ internal/platform/database/database.go
@@ -2,7 +2,10 @@ package database
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
+	gap "github.com/muesli/go-app-paths"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 
@@ -11,7 +14,32 @@ import (
 
 // Connect connects to the sqlite database and returns a DB connection.
 func Connect() (*gorm.DB, error) {
-	database, err := gorm.Open(sqlite.Open("work-pilot.db"), &gorm.Config{})
+	scope := gap.NewScope(gap.User, "work-pilot")
+
+	databasePath, err := scope.DataPath("logbook.db")
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize database scope path: %w", err)
+	}
+
+	dir := filepath.Dir(databasePath)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return nil, fmt.Errorf("failed to create database dir: %w", err)
+		}
+	}
+
+	if _, err := os.Stat(databasePath); os.IsNotExist(err) {
+		file, err := os.Create(databasePath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create database file: %w", err)
+		}
+
+		defer func() {
+			_ = file.Close()
+		}()
+	}
+
+	database, err := gorm.Open(sqlite.Open(databasePath), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database: %w", err)
 	}
